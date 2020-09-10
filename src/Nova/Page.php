6a5ac4b759e3aea4839c84bc208afbbb6fd0dd71
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-09-10 09:24:13 +0430

    Creates page resources

---
+++ src/Nova/Page.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Armincms\Blogger\Nova;
+
+use Armincms\Home\Nova\Resource;
+
+class Page extends Resource
+{  
+    /**
+     * The logical group associated with the resource.
+     *
+     * @var string
+     */
+    public static $group = 'Blog';
+}
