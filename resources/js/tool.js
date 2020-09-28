fcd0d93b89c7c03dae47d516adfd8347c7971c53
اسماعیل زارع <zarehesmaiel@gmail.com>
2020-09-28 14:28:37 +0330

    Removes unneccessary files

--- resources/js/tool.js
+++
@@ -1,9 +0,0 @@
-Nova.booting((Vue, router, store) => {
-  router.addRoutes([
-    {
-      name: 'blogger',
-      path: '/blogger',
-      component: require('./components/Tool'),
-    },
-  ])
-})
