26d38e1bb45a16a0bd00f7368573b5c861b21ed5
Ismail Zare <zarehesmaiel@gmail.com>
2021-01-12 13:33:29 +0330

    Added category to module locates

--- src/Locate.php
+++ src/Locate.php
@@ -13,17 +13,37 @@ class Locate
             ])->map(function($resource) {
                 return [
                     'title' => $resource::label(),
-                    'name' => Str::singular($resource::uriKey()),
-                    'id' => '*',
+                    'name'  => Str::singular($resource::uriKey()),
+                    'id'    => '*',
                     'childrens' => $resource::newModel()->resource($resource)->get()->mapInto($resource)->map(function($resource) { 
                         return [
                             'title' => $resource->title() ?? $resource->getKey(), 
-                            'name' => Str::singular($resource::uriKey()),
-                            'id' => $resource->getKey(),
-                            'url'    => $resource->url(),
+                            'name'  => Str::singular($resource::uriKey()),
+                            'id'    => $resource->getKey(),
+                            'url'   => $resource->url(),
                         ];
                     })->toArray(),
                 ];
-            })->values()->toArray();
+            })->push([
+                'title' => Nova\Category::label(),
+                'name'  => 'category',
+                'id'    => '*',
+                'childrens' => Nova\Category::newModel()->whereDoesntHave('parent')->with('subCategories')->get()->mapInto(Nova\Category::class)->map([static::class, 'categoryInformation'])->toArray()
+            ])->values()->toArray();
+    }
+
+    public static function categoryInformation($category)
+    {
+        $childrens = $category->subCategories->mapInto(Nova\Category::class)->map([
+            static::class, 'categoryInformation'
+        ]);
+
+        return array_filter([
+            'title' => $category->title() ?? $category->getKey(), 
+            'name'  => Str::kebab(class_basename($category::$model)),
+            'id'    => $category->getKey(),
+            'childrens' => $childrens->isEmpty() ? null : $childrens->all(),
+            'url'   => app('site')->get('blog')->url(urldecode($category->url)),
+        ]);
     }
 }
