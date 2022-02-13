38cf7db6a7ace76dc64dea824729a05f9d18b906
Ismail Zare <zarehesmaiel@gmail.com>
2022-02-13 11:49:00 +0330

    Updated tag widget correspond to the base widget

--- src/Cypress/Widgets/BlogTag.php
+++ src/Cypress/Widgets/BlogTag.php
@@ -7,16 +7,6 @@ use Armincms\Taggable\Cypress\Widgets\SingleTag;
 class BlogTag extends SingleTag
 {       
     /**
-     * Get the category related content template name.
-     * 
-     * @return string
-     */
-    public static function contentTemplateName(): string
-    {
-        return \Armincms\Blogger\Gutenberg\Templates\IndexPost::class;
-    } 
-
-    /**
      * Get the related model.
      * 
      * @param  string $relationship 
@@ -24,6 +14,60 @@ class BlogTag extends SingleTag
      */
     protected static function relationModel(string $relationship): string
     { 
-        return \Armincms\Blogger\Models\Post::class;
+        return \Armincms\Blogger\Models\Blog::class;
+    }
+  
+    /**
+     * Get the tag related content template name.
+     * 
+     * @return string
+     */
+    public static function resources(): array
+    {
+        return [
+            \Armincms\Blogger\Nova\Article::class,
+            \Armincms\Blogger\Nova\Podcast::class,
+            \Armincms\Blogger\Nova\Post::class,
+            \Armincms\Blogger\Nova\Video::class,
+        ];
+    } 
+  
+    /**
+     * Get the template handlers for given resourceName.
+     * 
+     * @return string
+     */
+    public static function handler(string $resourceName): array
+    {
+        return [
+            'Armincms\\Blogger\\Gutenberg\\Templates\\Index'. class_basename($resourceName)
+        ];
+    } 
+
+    /**
+     * Apply custom query to the relationship query.
+     * 
+     * @param  \Illuminate\Database\Eloquent\Builder  $query        
+     * @param  string  $relationship 
+     * @return \Illuminate\Database\Eloquent\Builder                
+     */
+    protected function applyRelationshipQuery($query, $relationship)
+    {
+        $resources = collect($this->resources())->filter(function($resource) {
+            return $this->metaValue($resource::uriKey());
+        });
+
+        return $query->resources($resources->values()->all());
+    }
+
+    /**
+     * Get resource for the given model.
+     * 
+     * @param  \Illuminate\Database\Eloqeunt\Model $model 
+     * @return string      
+     */
+    public static function findResourceForModel($model)
+    {
+        return $model->resource;
     }
 }
